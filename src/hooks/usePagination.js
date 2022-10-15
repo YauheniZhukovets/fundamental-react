9028bd490def6e68fdcafc35d2dfa40ad218ef04
zhukavets <stewart-8@mail.ru>
2022-10-15 21:13:29 +0300

    added AuthContext

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,8 +1,9 @@
 import {useMemo} from "react";
 
 export const usePagination = (totalPage) => {
+
     return useMemo(() => {
-        let arr = [...new Array(totalPage)]
+        let arr = [...new Array(Math.abs(totalPage))]
         return arr.map((el, i) => i + 1)
     }, [totalPage])
 }
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react';
-import {PostList} from "../Components/PostList";
+import React, {useEffect, useRef, useState} from 'react';
 import {usePosts} from "../hooks/usePosts";
 import {useFetching} from "../hooks/useFetching";
 import PostService from "../API/postService";
@@ -8,8 +7,10 @@ import {Button} from "../Components/UI/button/Button";
 import {Modal} from "../Components/UI/modal/Modal";
 import {PostForm} from "../Components/PostForm";
 import {PostFilter} from "../Components/PostFilter";
-import {Pagination} from "../Components/UI/pagination/Pagination";
 import {Loader} from "../Components/UI/loder/Loader";
+import {PostList} from "../Components/PostList";
+import {useObserver} from "../hooks/useObserver";
+import {Select} from "../Components/UI/select/Select";
 
 
 export const Posts = () => {
@@ -18,21 +19,24 @@ export const Posts = () => {
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const [totalPage, setTotalPage] = useState(0)
-    const [limit, seLimit] = useState(10)
+    const [limit, seLimit] = useState(5)
     const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const lastElement = useRef()
 
     const [fetchPost, isPostLoading, errorFetchPost] = useFetching(async () => {
         const res = await PostService.getAll(limit, page)
-        setPosts(res.data)
+        setPosts([...posts, ...res.data])
 
         const totalCount = res.headers['x-total-count']
         setTotalPage(getPageCount(totalCount, limit))
     })
 
+    useObserver(lastElement, page < totalPage, isPostLoading, () => setPage(page + 1))
+
     useEffect(() => {
         fetchPost()
-    }, [page])
+    }, [page, limit])
 
 
     const changePage = (page) => {
@@ -67,32 +71,40 @@ export const Posts = () => {
                         setFilter={setFilter}
             />
 
+            <div style={{marginTop: 5}}>
+                <Select value={limit}
+                        onChange={value => seLimit(value)}
+                        defaultValue={'Кол-во элементов'}
+                        options={[
+                            {value: 5, name: '5'},
+                            {value: 8, name: '8'},
+                            {value: 10, name: '10'},
+                            {value: -1, name: 'Показать все'},
+                        ]}
+                />
+            </div>
+
+
             {
                 errorFetchPost &&
                 <h1>Произошла ошибка: {errorFetchPost}</h1>
             }
 
-            <Pagination currentPage={page}
+            <PostList deletePost={deletePost}
+                      posts={sortedAndSearchedPosts}
+                      title={'Список постов'}
+            />
+            <div ref={lastElement}></div>
+
+            {isPostLoading
+                && <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <Loader/>
+                </div>}
+
+            {/*<Pagination currentPage={page}
                         totalPage={totalPage}
                         onClickCallback={changePage}
-            />
-
-            {
-                isPostLoading
-                    ? <div style={{display: 'flex', justifyContent: 'center'}}>
-                        <Loader/>
-                    </div>
-                    : <PostList deletePost={deletePost}
-                                posts={sortedAndSearchedPosts}
-                                title={'Список постов'}
-                    />
-
-            }
-
-            <Pagination currentPage={page}
-                        totalPage={totalPage}
-                        onClickCallback={changePage}
-            />
+            />*/}
 
         </div>)
 }
